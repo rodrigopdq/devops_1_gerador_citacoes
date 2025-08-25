1322dfc533ef14b07da78322ba70240a51a9a7fc
Rodrigo <rodrigopdq@live.com>
2025-08-25 14:04:48 -0300

    feat: Adiciona mais citações à lista

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const citacoes = [ 
 "A imaginação é mais importante que o conhecimento. - Albert Einstein", 
+"O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill" 
 ]; 
