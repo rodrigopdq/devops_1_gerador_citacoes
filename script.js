6abc8f39cccd7e4f890ada34a952474356afc3d6
Rodrigo <rodrigopdq@live.com>
2025-08-25 14:05:39 -0300

    feat: Implementa lógica do botão para gerar citação aleatória

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const citacoes = [
 "A imaginação é mais importante que o conhecimento. - Albert Einstein", 
 "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill" 
 ]; 
+
+const citacaoEl = document.getElementById("citacao"); 
+const botaoEl = document.getElementById("novaCitacaoBtn"); 
+botaoEl.addEventListener("click", () => { 
+const randomIndex = Math.floor(Math.random() * citacoes.length); 
+citacaoEl.textContent = citacoes[randomIndex]; 
+});
